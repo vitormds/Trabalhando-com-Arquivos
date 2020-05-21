018fa71c4b047730ffda3e1b4f23b8551b29c245
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-05-21 20:07:39 -0300

    estabelecendo a entrada

--- works-arquivos/java-io/src/br/com/java/io/teste/TesteLeitura.java
+++ works-arquivos/java-io/src/br/com/java/io/teste/TesteLeitura.java
@@ -1,10 +1,22 @@
 package br.com.java.io.teste;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TesteLeitura {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		// Fluxo de entrada com Arquivo
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader bf = new BufferedReader(isr);
+		String linha = bf.readLine();
+		System.out.println(linha);
+		bf.close();
+		
 	}
 
 }
