9b529f4dd5c9e19dd14d0ffe7b965a8049896bbe
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-05-23 16:54:45 -0300

    Passeando arquivos

--- works-arquivos/java-io/src/br/com/java/io/teste/TesteLeitura2.java
+++ works-arquivos/java-io/src/br/com/java/io/teste/TesteLeitura2.java
@@ -2,6 +2,7 @@ package br.com.java.io.teste;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -11,6 +12,22 @@ public class TesteLeitura2 {
 		while(scanner.hasNext()) {
 			String linha = scanner.nextLine();
 			System.out.println(linha);
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String v1 = linhaScanner.next();
+			int v2 = linhaScanner.nextInt();
+			int v3 = linhaScanner.nextInt();
+			String v4 = linhaScanner.next();
+			Double v5 = linhaScanner.nextDouble();
+			
+			System.out.println(v1 + v2 + v3 + v4 + v5);
+			linhaScanner.close();
+
+			//String[] valores = linha.split(",");
+			//System.out.println(valores[0]);
+ 			
 		}
 		scanner.close();
 		
