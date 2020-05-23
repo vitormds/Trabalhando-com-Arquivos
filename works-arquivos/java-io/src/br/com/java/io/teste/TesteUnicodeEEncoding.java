0d6651a0af5c13b3fd435d76dd2918efdaf0adbf
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-05-23 18:22:50 -0300

    Encoding no Windows

---
+++ works-arquivos/java-io/src/br/com/java/io/teste/TesteUnicodeEEncoding.java
@@ -0,0 +1,33 @@
+package br.com.java.io.teste;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class TesteUnicodeEEncoding {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String s = "ç";
+		System.out.println(s.codePointAt(0));
+		
+		Charset c = Charset.defaultCharset();
+		System.out.println(c.displayName());
+		
+		byte[] bytes = s.getBytes("windows-1252");
+		System.out.print(bytes.length + ", windowns-1252, ");
+		String sNovo = new String(bytes);
+		System.out.println(sNovo);
+
+		
+		    bytes = s.getBytes("UTF-16");
+			System.out.print(bytes.length + ", UTF-16, ");
+			 sNovo = new String(bytes, "UTF-16");
+			System.out.println(sNovo);
+			
+			bytes = s.getBytes(StandardCharsets.US_ASCII);
+			System.out.print(bytes.length + ", US_ASCII, " );
+			 sNovo = new String(bytes);
+			System.out.println(sNovo);
+	}
+
+}
