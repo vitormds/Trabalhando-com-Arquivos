7cbc5e1400ede6b2afdaf798613373db477d729e
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-05-23 16:36:21 -0300

    Leitura com scanner

---
+++ works-arquivos/java-io/src/br/com/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,19 @@
+package br.com.java.io.teste;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		while(scanner.hasNext()) {
+			String linha = scanner.nextLine();
+			System.out.println(linha);
+		}
+		scanner.close();
+		
+	}
+
+}
